70d2fc11f79ee0739bd75b9ff95375f19428be10
Nico Almiron <almiron.nicolas16@gmail.com>
2024-01-29 12:49:13 -0300

    se creo la estructura del proyecto y se cambio el icono de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Homepage from "./component/views/Homepage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Homepage></Homepage>
+    </>
+  );
 }
 
 export default App;
