30bca56a262e2c912883caa69ce698ee6a52c8cb
Nico Almiron <almiron.nicolas16@gmail.com>
2024-12-28 21:10:27 -0300

    se realizo el maquetado del footer (sin agregar las Redes Sociales)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.css";
 import Homepage from "./component/views/Homepage";
 import Menu from "./component/common/Menu.jsx";
+import Footer from "./component/common/Footer.jsx";
 
 function App() {
   return (
     <>
       <Menu></Menu>
       <Homepage></Homepage>
+      <Footer></Footer>
     </>
   );
 }
--- src/component/common/Footer.jsx
+++ src/component/common/Footer.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import "./../../css/footer.css";
+import { Button, Col, Row } from "react-bootstrap";
+//import { Button } from "bootstrap";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="footer-bottom">
+      <Row className="background-footer">
+        <Col className="col-4">
+          <div className="mt-2">
+            <img
+              src="/public/logotipo-v2.png"
+              alt="logotipo-blue-novedades-version-2"
+              className="w-100"
+            />
+          </div>
+        </Col>
+        <Col className="col-4">
+          <div className="text-center p-2">
+            <Button className="my-1 btn-footer">Inicio</Button>
+            <Button className="my-1 btn-footer">Productos</Button>
+            <Button className="my-1 btn-footer">Contacto</Button>
+          </div>
+        </Col>
+        <Col className="col-4">
+          <div className="p-3 text-center">
+            <h4 className="font-footer">Redes Sociales</h4>
+          </div>
+        </Col>
+      </Row>
+    </footer>
   );
 };
 
 export default Footer;
-<h1>Footer</h1>;
